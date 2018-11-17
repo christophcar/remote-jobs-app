77f2267a1da69eb0feef81befc2d297e2b9f1062
Christoph <carstens.christoph@gmail.com>
2018-11-17 12:57:23 +0100

    Initial commit

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -8,7 +8,7 @@ export default createGlobalStyle`
 
 body {
   margin: 0;
-  padding: 0;
+  padding: 10px;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
