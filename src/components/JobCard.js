b138f45e7a9d3f772b3c91833eb5499696f17971
Christoph <carstens.christoph@gmail.com>
2018-11-26 14:16:45 +0100

    Bug fixes

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 const Card = styled(Link)`
   display: grid;
   grid-template-columns: 1fr 4fr 2fr;
-  grid-template-rows: 65px 65px;
+  grid-template-rows: 45px 45px;
   align-items: center;
   margin: 25px auto;
   padding: 30px;
@@ -21,14 +21,13 @@ const Card = styled(Link)`
 `
 const Position = styled.h2`
   font-size: 22px;
-  line-height: 1.3;
+  line-height: 1;
   grid-column: 2 / 3;
   margin-left: 15px;
 `
 
 const Company = styled.h3`
   font-size: 23px;
-  line-height: 1.3;
   font-weight: 400;
   grid-column: 2 / 3;
   margin-left: 15px;
