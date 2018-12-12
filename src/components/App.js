542f86ab1fe73489624f5de0f6bc92f7bd772132
Christoph <carstens.christoph@gmail.com>
2018-12-12 19:34:29 +0100

    Refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from 'redux-starter-kit'
 import HomeContainer from './HomeContainer'
 import NavigationContainer from './NavigationContainer'
 import Loading from './Loading'
+import SubPageContainer from './SubPageContainer'
 import styled from 'styled-components'
 import jobs from '../stepstone.json'
-import { configureStore } from 'redux-starter-kit'
 import reducer from '../ducks/reducer'
-import { Provider } from 'react-redux'
-import SubPageContainer from './SubPageContainer'
 
 const Container = styled.section`
   margin-top: 130px;
